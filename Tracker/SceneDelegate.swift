7921f215a44f46ac54a16688877bfc1a476a2600
Brabus098 <vovan1998888@mail.ru>
2024-05-01 12:00:00 +0300

    [Refactor] Update SceneDelegate configuration

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -18,22 +18,5 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = startController
         window?.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
-
-
 }
 
