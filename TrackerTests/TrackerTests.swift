f7feef0a0ca4dbf44fe37bf62b74019cbc0915a6
Brabus098 <vovan1998888@mail.ru>
2025-10-02 18:57:21 +0300

    Добавлены скриншот-тесты для светлой/темной темы.

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -9,7 +9,29 @@ final class AlpabetTests: XCTestCase {
     
     func testViewController() {
         setupTabBarAppearanceForTests()
+        let tab = setUpControllers()
+        assertSnapshot(of: tab, as: .image(traits: .init(userInterfaceStyle: .light)))
+    }
+    
+    func testBlackViewController() {
+        setupTabBarAppearanceForTests()
+        let tab = setUpControllers()
+        assertSnapshot(of: tab, as: .image(traits: .init(userInterfaceStyle: .dark)))
+    }
+    
+    private func setUpControllers() -> UITabBarController {
         
+        let trackNavigationController = setupPlusController()
+        let statisticController = setupStatisticController()
+        let tabBarController = setupTabBar(firstController: trackNavigationController, secondController: statisticController)
+        tabBarController.tabBar.tintColor = .red // для теста светлой/темной темы
+        
+        XCTAssertNotNil(UIImage(named: "statisticTabBarLogo"))
+        
+        return tabBarController
+    }
+    
+    private func setupPlusController() -> PlusNavigationController {
         let model = RegularTrackModel()
         let storeReader = TrackerStoreReader()
         let recordStore = TrackerRecordStore()
@@ -22,31 +44,46 @@ final class AlpabetTests: XCTestCase {
             image: UIImage(named: "tracTabBarLogo"),
             selectedImage: UIImage(named: "tracTabBarLogo"))
         
-        let trackNavigationController = PlusNavigationController(rootViewController: trackController)
+        return PlusNavigationController(rootViewController: trackController)
+    }
+    
+    private func setupStatisticController() -> StatisticController {
         
-        let statisticController = StatisticController()
-        
-        statisticController.tabBarItem = UITabBarItem(
+        let controller = StatisticController()
+        controller.tabBarItem = UITabBarItem(
             title: String(localized: "Statistics"),
             image: UIImage(named: "statisticTabBarLogo"),
             selectedImage: UIImage(named: "statisticTabBarLogo"))
         
+        return controller
+    }
+    
+    private func setupTabBar(firstController: PlusNavigationController, secondController: StatisticController) -> UITabBarController {
         let tabBarController = UITabBarController()
-        tabBarController.setViewControllers([trackNavigationController, statisticController], animated: true)
-        
+        let color = Colors()
+
+        tabBarController.setViewControllers([firstController, secondController], animated: true)
+
         tabBarController.additionalSafeAreaInsets = UIEdgeInsets(top: 30, left: 0, bottom: 30, right: 0)
         
-        tabBarController.tabBar.layer.masksToBounds = true
-        tabBarController.tabBar.layer.borderWidth = 1
-        tabBarController.tabBar.layer.borderColor = UIColor.lightGray.cgColor
-        
-        XCTAssertNotNil(UIImage(named: "statisticTabBarLogo"))
-        
-        assertSnapshot(of: tabBarController, as: .image)
+        let borderView = UIView()
+        borderView.backgroundColor = color.colorForSeparator
+        borderView.translatesAutoresizingMaskIntoConstraints = false
+        tabBarController.tabBar.addSubview(borderView)
+
+        NSLayoutConstraint.activate([
+            borderView.topAnchor.constraint(equalTo: tabBarController.tabBar.topAnchor),
+            borderView.leadingAnchor.constraint(equalTo: tabBarController.tabBar.leadingAnchor),
+            borderView.trailingAnchor.constraint(equalTo:tabBarController.tabBar.trailingAnchor),
+            borderView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+        return tabBarController
     }
+    
     private func setupTabBarAppearanceForTests() {
         // Принудительно для всех tab bar в тестах
         UITabBar.appearance().tintColor = .systemBlue
         UITabBar.appearance().unselectedItemTintColor = .systemGray
     }
+
 }
